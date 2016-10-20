c619eba9acde0ec83ec0397a7c357f1e3aa88669
henrirespaudbouny <henrirespaudbouny@gmail.com>
2016-10-20 23:17:13 +0200

    TD3 Finished

--- address_book_build/main.cpp
+++ address_book_build/main.cpp
@@ -20,7 +20,7 @@ class Menu{
 	static int goodbyeMessage(int *loop){
 		*loop = 0;
 		std::cout << "-------------------------------------------------------------------" << std::endl;
-		std::cout << "|   Thank you for using my Address Book program! See you soon!    |                |" << std::endl;
+		std::cout << "|   Thank you for using my Address Book program! See you soon!    |" << std::endl;
 		std::cout << "-------------------------------------------------------------------" << std::endl;
 		return *loop;
 	}
@@ -28,10 +28,9 @@ class Menu{
 	static void displayMainMenu(){
 		std::cout << "MAIN MENU" << std::endl;
 		std::cout << "-------------------------------------------------------------------" << std::endl;
-		std::cout << "1) Show Contacts  	2) Search Contacts" << std::endl;
-		std::cout << "3) Add Contact  	4) Remove Contact" << std::endl;
-		std::cout << "5) Edit Contact  	6) Export" << std::endl;
-		std::cout << "7) Exit" << std::endl;
+		std::cout << "1) SHOW  	2) SEARCH" << std::endl;
+		std::cout << "3) ADD  	4) SAVE FILE" << std::endl;
+		std::cout << "5) EXIT"	<< std::endl;
 		std::cout << "-------------------------------------------------------------------" << std::endl;
 	}
 
@@ -43,11 +42,58 @@ class Menu{
 	}
 	
 	static int callToActionMenu(){
-		std::string answer;
-		
+		std::string answer;		
+
 		std::cout << "1) ADD  	2) EXIT" << std::endl;
 		std::cout << "YOUR CHOICE :";
-		std::cin.ignore();
+		getline(std::cin, answer);
+		int int_answer = std::stoi(answer, 0, 10);
+		
+		return int_answer;
+	}
+
+	static int callToActionMenuWithContact(){
+		std::string answer;		
+
+		std::cout << "1) SHOW MORE 		2) ADD    	3) REMOVE   	4) SHOW CONTACT		5) EXIT" << std::endl;
+		std::cout << "YOUR CHOICE :";
+		getline(std::cin, answer);
+		int int_answer = std::stoi(answer, 0, 10);
+		
+		return int_answer;
+	}
+
+	static int showMore(){
+		std::string answer;		
+
+		std::cout << "PLEASE CHOOSE CONTACT (corresponding figure)" << std::endl;
+		std::cout << "YOUR CHOICE :";
+		getline(std::cin, answer);
+		int int_answer = std::stoi(answer, 0, 10);
+		
+		return int_answer;
+	}
+
+	static int actionMenuShowMore(){
+		std::string answer;		
+
+		std::cout << "1) EDIT 		2) REMOVE 		3) EXIT" << std::endl;
+		std::cout << "YOUR CHOICE :";
+		getline(std::cin, answer);
+		int int_answer = std::stoi(answer, 0, 10);
+		
+		return int_answer;
+	}
+
+	static int menuEdit(){
+		std::string answer;		
+
+		std::cout << "WHAT DO YOU WANT TO EDIT ?" << std::endl << std::endl;
+		std::cout << "1) FIRSTNAME 		2) NAME" << std::endl;
+		std::cout << "3) AGE 			4) ADDRESS" << std::endl;
+		std::cout << "5) MAIL 		6) PHONE" << std::endl;
+		std::cout << "7) NOTHING" << std::endl;
+		std::cout << "YOUR CHOICE :";
 		getline(std::cin, answer);
 		int int_answer = std::stoi(answer, 0, 10);
 		
@@ -96,18 +142,27 @@ class Address{
 		std::string getCountry(){return country;}
 
 		//PRINT
-		void printStreetnumber(){std::cout << streetnumber << std::endl;}
-		void printStreetname(){std::cout << streetname << std::endl;}
-		void printZipcode(){std::cout << zipcode << std::endl;}
-		void printCity(){std::cout << city << std::endl;}
-		void printCountry(){std::cout << country << std::endl;}
+		void printStreetnumber(){std::cout << streetnumber;}
+		void printStreetname(){std::cout << streetname;}
+		void printZipcode(){std::cout << zipcode;}
+		void printCity(){std::cout << city;}
+		void printCountry(){std::cout << country;}
+
+		std::string getAddress() const{
+			std::string streetnb = std::to_string(streetnumber);
+			return streetnb + " " + streetname + " " + zipcode + " " + city+ " " + country;
+		}
 
 			
 		void printAddress(){
 			printStreetnumber();
+			std::cout << " ";
 			printStreetname();
+			std::cout << std::endl;
 			printZipcode();
+			std::cout << " ";
 			printCity();
+			std::cout << std::endl;
 			printCountry();
 		}
 
@@ -145,7 +200,6 @@ class Address{
 
 				return new_address;
 		}
-
 };
 
 class Person{
@@ -207,20 +261,26 @@ class Person{
 
 	
 		//PRINT
-			void printFirstName(){std::cout << firstname << std::endl;}
-			void printName(){std::cout << 	   name << std::endl;}
-			void printAge(){std::cout << 	   age << std::endl;}
-			void printEmail(){std::cout << 	   email << std::endl;}
-			void printPhone(){std::cout << 	   phone << std::endl;}
+			void printFirstName(){std::cout << firstname;}
+			void printName(){std::cout << 	   name;}
+			void printAge(){std::cout << 	   age;}
+			void printEmail(){std::cout << 	   email;}
+			void printPhone(){std::cout << 	   phone;}
 
 			
 			static void printPerson(Person contact){
 				contact.printFirstName();
+				std::cout <<" ";
 				contact.printName();
+				std::cout << std::endl;
 				contact.printAge();
+				std::cout <<" years old" << std::endl;
 				contact.address.Address::printAddress();
+				std::cout << std::endl;
 				contact.printEmail();
+				std::cout << std::endl;
 				contact.printPhone();
+				std::cout << std::endl;
 			};
 
 			
@@ -233,7 +293,7 @@ class Person{
 				std::cout << std::endl;
 				std::cout << std::endl;
 				
-				std::cout << "=================ADD CONTACT FORM==================" << std::endl;
+				std::cout << "============================ ADD CONTACT FORM =============================" << std::endl;
 				std::cout << "Please enter Contact Information" << std::endl;
 				std::cout << std::endl;
 
@@ -267,27 +327,152 @@ class Person{
 				contact_list.push_back(new_contact);
 			}
 
+			
+			//SEARCH
+			static void findContactByFirstName(std::vector<Person> &contact_list){
+				std::string search;
+				unsigned int result = 0;
+
+				std::cout << "===============================SEARCH===============================" << std::endl;
+
+				std::cout << "Please Enter First name to search:";
+				getline(std::cin, search);
+
+				std::cout << std::endl;
+				
+				for (unsigned int i = 0 ; i < contact_list.size() ; i++){
+					std::cout << "=================================================================" << std::endl;
+					if(search == contact_list[i].firstname){
+						result++;
+						std::cout << result << "." << std::endl;
+						printPerson(contact_list[i]);
+					}
+				}
+				std::cout << "=====================================================================" << std::endl;
+				if(!result)
+					std::cout << "NO MATCH !" << std::endl;
+
+			}	
+
+
+			//REMOVE
+			static void removeContact(std::vector<Person> &contact_list, int index){
+				contact_list.erase(contact_list.begin()+(index-1));
+				std::cout << "CONTACT DELETED !" << std::endl;
+			}
+
+			//EDIT
+			static void editFirstName(std::vector<Person> &contact_list, int index){
+
+				std::string answer;
+
+				std::cin.ignore();
+
+				std::cout << "Enter New First name :";
+				getline(std::cin, answer);
+				contact_list[index-1].firstname = answer;
+				std::cout << std::endl;
+
+				std::cout << "EDIT !" << std::endl;
+			}
+
+
+			static void editName(std::vector<Person> &contact_list, int index){
+
+				std::string answer;
+
+				std::cin.ignore();
+
+				std::cout << "Enter New Name :";
+				getline(std::cin, answer);
+
+				contact_list[index-1].name = answer;
+				std::cout << std::endl;
+
+				std::cout << "EDIT !" << std::endl;
+			}
+
+			static void editAge(std::vector<Person> &contact_list, int index){
+
+				std::string answer;
+
+				std::cin.ignore();
+
+				std::cout << "Enter New Age :";
+				getline(std::cin, answer);
+				contact_list[index-1].age = std::stoi(answer, 0, 10);;
+				std::cout << std::endl;
+
+				std::cout << "EDIT !" << std::endl;
+			}
+
+			static void editAddress(std::vector<Person> &contact_list, int index){
+
+				std::string answer;
+
+				std::cin.ignore();
+
+				std::cout << "Enter New Address :";
+				contact_list[index-1].address = Address::addAddress();
+				std::cout << std::endl;
+
+				std::cout << "EDIT !" << std::endl;
+			}
+
+			static void editEmail(std::vector<Person> &contact_list, int index){
+
+				std::string answer;
+
+				std::cin.ignore();
+
+				std::cout << "Enter New Email :";
+				getline(std::cin, answer);
+
+				contact_list[index-1].email = answer;
+				std::cout << std::endl;
+
+				std::cout << "EDIT !" << std::endl;
+			}
+
+			static void editPhone(std::vector<Person> &contact_list, int index){
+
+				std::string answer;
+
+				std::cin.ignore();
+
+				std::cout << "Enter New Phone :";
+				getline(std::cin, answer);
+
+				contact_list[index-1].email = answer;
+				std::cout << std::endl;
+
+				std::cout << "EDIT !" << std::endl;
+			}
+
 			//SHOW
 			static void showContacts(std::vector<Person> &contact_list){
 				int menu_answer = 1;
+				int index;
+				int begin = 0;
+				unsigned int max = 10;
 				
 				std::cout << std::endl;
 				std::cout << std::endl;
 				std::cout << std::endl;
 
-				std::cout << "==============LIST OF YOUR CONTACT(S)==============" << std::endl;
+				std::cout << "===================LIST OF YOUR CONTACT(S)========================" << std::endl;
 
 				if(contact_list.size() < 1) {
 					std::cout << std::endl;
 					std::cout << "No Contact(s) yet !" << std::endl;
 					std::cout << std::endl;
-					std::cout << "===================================================" << std::endl;
+					std::cout << "=================================================================" << std::endl;
 					
 						while(menu_answer){
 							menu_answer = Menu::callToActionMenu();
 							if( menu_answer == 1 ){
 								Person::addContact(contact_list);
-								std::cout << "===================================================" << std::endl;
+								std::cout << "=================================================================" << std::endl;
 							}
 							else if( menu_answer == 2 )
 								return;
@@ -296,60 +481,92 @@ class Person{
 						}
 				}
 				else{
-					for (unsigned int i = 0 ; i < contact_list.size() ; i++){
-						std::cout 	<< i+1 << "." << contact_list[i].firstname << std::endl;
+					while(menu_answer){
+
+							if((contact_list.size()-max) > max)
+								max = contact_list.size();
+
+							for (unsigned int i = begin ; i < max ; i++){
+								std::cout 	<< i+1 << "." << contact_list[i].firstname << " " << contact_list[i].name << std::endl;
+							}
+
+							std::cout << "=================================================================" << std::endl;
+							menu_answer = Menu::callToActionMenuWithContact();
+							if( menu_answer == 1 ){
+								if(contact_list.size()>10){
+									max +=10;
+									begin +=10;
+								}
+								else{
+									std::cout << "ERROR YOU DON'T HAVE MORE CONTACTS" << std::endl;
+									Person::showContacts(contact_list);
+								}	
+							}
+							else if( menu_answer == 2 ){
+								Person::addContact(contact_list);
+								std::cout << "=================================================================" << std::endl;
+								menu_answer = Menu::callToActionMenu();
+							}
+							else if( menu_answer == 3 ){
+								std::cout << "============================= DELETE =============================" << std::endl;
+								menu_answer = Menu::showMore();
+								index = menu_answer;
+								Person::removeContact(contact_list, index);								
+								menu_answer = Menu::callToActionMenu();
+
+								std::cout << "=================================================================" << std::endl;
+							}
+							else if( menu_answer == 4 ){
+								std::cout << "============================ CONTACT ============================" << std::endl;
+								menu_answer = Menu::showMore();
+								index = menu_answer;
+								Person::printPerson(contact_list[index-1]);
+								menu_answer = Menu::actionMenuShowMore();
+								std::cout << "=================================================================" << std::endl;
+
+									if( menu_answer == 1 ){
+										std::cout << "============================= EDIT =============================" << std::endl;
+										menu_answer = Menu::menuEdit();
+											if( menu_answer == 1 )
+												editFirstName(contact_list, index);
+											else if( menu_answer == 2 )
+												editName(contact_list, index);
+											else if( menu_answer == 3 )
+												editAge(contact_list, index);
+											else if( menu_answer == 4 )
+												editAddress(contact_list, index);
+											else if( menu_answer == 5 )
+												editEmail(contact_list, index);
+											else if( menu_answer == 6 )
+												editPhone(contact_list, index);
+											else if( menu_answer == 7 )
+												return;
+											else
+												std::cout << "ERROR PLEASE CHOOSE AGAIN !" << std::endl;
+									}
+									else if (menu_answer == 3){
+										std::cout << "============================= DELETE =============================" << std::endl;
+										menu_answer = Menu::showMore();
+										index = menu_answer;
+										Person::removeContact(contact_list, index);								
+										menu_answer = Menu::callToActionMenu();
+										std::cout << "=================================================================" << std::endl;
+									}
+									else if (menu_answer == 3)
+										return;
+									else
+										std::cout << "ERROR PLEASE CHOOSE AGAIN !" << std::endl;
+							}
+							else if( menu_answer == 5 )
+								return;
+							else
+								std::cout << "ERROR PLEASE CHOOSE AGAIN !" << std::endl;		
 					}
 				}
 				
 				std::cout << std::endl;
 				std::cout << std::endl;
 				std::cout << std::endl;
-			}
-
-			//SEARCH
-			static void findContactByFirstName(std::vector<Person> &contact_list){
-				std::string search;
-				unsigned int result = 0;
-
-				std::cin.ignore();
-
-				std::cout << "Enter First name :";
-				getline(std::cin, search);
-				
-				for (unsigned int i = 0 ; i < contact_list.size() ; i++){
-					if(search == contact_list[i].firstname){
-						result++;
-						printPerson(contact_list[i]);
-					}
-				}
-
-				if(!result)
-					std::cout << "NO MATCH !" << std::endl;
-
-			}	
-
-
-			//REMOVE
-			static void removeContact(std::vector<Person> &contact_list){
-				int index = 4;
-				contact_list.erase(contact_list.begin()+(index));
-				std::cout << "DELETE !" << std::endl;
-			}
-
-			//EDIT
-			static void editContact(std::vector<Person> &contact_list){
-
-				int index=0;
-				std::string answer;
-
-				std::cin.ignore();
-
-				std::cout << "Enter New First name :";
-				getline(std::cin, answer);
-				contact_list[index].firstname = answer;
-				std::cout << std::endl;
-
-				std::cout << "EDIT !" << std::endl;
 			}
 
 			//EXPORT
@@ -358,7 +575,8 @@ class Person{
 				std::string filename;
 				std::ofstream outfile;
 
-				std::cin.ignore();
+				std::string address;
+
 				std::cout<<"Enter file name (.txt): ";
 
 				getline(std::cin,filename);
@@ -366,11 +584,24 @@ class Person{
 
 
 				if (!outfile.fail()){
-					std::cout << "Saving Address Book to the disc ";
+					std::cout << "Saving Address Book to the disc " << std::endl;
 
+
+					outfile << "===============CONTACT(S)=====================" << std::endl;
 					for(unsigned int i = 0; i < contact_list.size(); i++){
-						outfile << contact_list[i].firstname;
 
+							outfile << i+1 << "." << std::endl;
+							outfile << contact_list[i].firstname << " " << contact_list[i].name << std::endl;
+							outfile << contact_list[i].age << "years old" << std::endl;
+
+							address = contact_list[i].address.Address::getAddress();
+							outfile << address << std::endl;
+
+
+							outfile << contact_list[i].email << std::endl;
+							outfile << contact_list[i].phone << std::endl;
+
+							outfile << "============================================" << std::endl;
 						if (i < contact_list.size()-1) outfile << std::endl;
 					}
 
@@ -399,25 +630,22 @@ int main(){
 		Menu::displayMainMenu();
 		menu_answer = Menu::actionMainMenu();
 
+		std::cin.ignore();
+
 		if(menu_answer == 1)
 			Person::showContacts(contact_list);
 
-		if(menu_answer == 2)
+		if(menu_answer == 2){
 			Person::findContactByFirstName(contact_list);
+		}
 
 		if(menu_answer == 3)
 			Person::addContact(contact_list);
 
 		if(menu_answer == 4)
-			Person::removeContact(contact_list);
-
-		if(menu_answer == 5)
-			Person::editContact(contact_list);
-
-		if(menu_answer == 6)
 			Person::saveFile(contact_list);
 
-		if(menu_answer == 7)
+		if(menu_answer == 5)
 			Menu::goodbyeMessage(&loop);
 	}
 	
